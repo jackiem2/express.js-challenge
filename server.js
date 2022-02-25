a069f7aa6d2001fa2f90e6600ab75c915d8fe7e9
jackiem2 <jacqueline.merlan@gmail.com>
2022-02-24 20:19:02 -0600

    added app post

--- server.js
+++ server.js
@@ -26,13 +26,19 @@ app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
-});
 
 function createNewNote(body, notesArray) {
     const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
     notesArray.push(newNote);
+
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify({newnNote: notesArray}, null, 2)
@@ -41,5 +47,13 @@ function createNewNote(body, notesArray) {
 }
 
 app.post('/api/notes', (req,res) => {
-    
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
 })
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
