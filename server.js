4efc24f84b78fe071f1effb2831bc50f2501a117
jackiem2 <jacqueline.merlan@gmail.com>
2022-02-24 20:55:20 -0600

    updated app post

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
-const allNotes = require('./db/db.json');
+
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -27,28 +27,15 @@ app.get('*', (req,res) => {
 });
 
 
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-    notesArray.push(newNote);
-
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({newnNote: notesArray}, null, 2)
-    );
-    return newNote;
-}
 
 app.post('/api/notes', (req,res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+    newNote.id = notelength;
+    noteList.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
 })
 
 
