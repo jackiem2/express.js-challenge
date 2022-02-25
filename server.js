a31db2c4dd8ae6e1cc46f99e801d07cfad76cae9
jackiem2 <jacqueline.merlan@gmail.com>
2022-02-24 19:47:58 -0600

    added get functions

---
+++ server.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+const allNotes = require('./db/db.json');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({newnNote: notesArray}, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req,res) => {
+    
+})
